dec1a082081f8ab01857aa4b20896183b8e8f5a9
Eduardo <eefs93@hotmail.com>
2025-08-23 11:39:57 -0500

    Se agrega compatibilidad con CORS

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import userRoutes from './routes/userRoutes.js'
+import { cors } from 'hono/cors'
+
+
+
+
 
 const app = new Hono()
+app.use('*', cors());
 app.route('/users', userRoutes)
 
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello juan!')
 })
 
 serve({
